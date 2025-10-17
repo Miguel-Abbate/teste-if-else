8ce4c1d743c0c46f844fe5250835fed9bd3affe2
Miguel-Abbate <abbatejr@gmail.com>
2025-10-17 09:27:06 -0300

    solucionado

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear(); 
+Console.Write("Nota da P1: ");
+string um = Console.ReadLine()!;
+double p1 = Convert.ToDouble(um);
+Console.Write("Nota da P2: ");
+string dois = Console.ReadLine()!;
+double p2 = Convert.ToDouble(dois);
+double media = (p1 + p2) / 2;
+
+
+if (media >= 5)
+{
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"Média: [{media}]");
+Console.WriteLine("Aprovado  ;)");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"Média: [{media}]");
+Console.WriteLine("Reprovado  */*");
+}
+Console.ResetColor();
